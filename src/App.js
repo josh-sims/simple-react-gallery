4f9c390f388dff9ccfebcd574f1dbb159bdb7e8f
josh SIms <josh.sims@realpage.com>
2019-05-20 11:29:26 -0500

    readme update + app.js refactor to json
    readme update + app.js refactor to json list for the cars

--- src/App.js
+++ src/App.js
@@ -13,13 +13,27 @@ function PageTitle(props) {
 
 // var cartoggle = true;
 var url = '';
-var ci = localStorage.getItem("curr_bgimg");
+var ci = localStorage.getItem("curr_bgimg") ? localStorage.getItem("curr_bgimg") : "url(images/full/2880-1800-crop-ferrari-250-gt-coupe-pininfarina-ii-c804316082018194649_1.jpg)";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bgimg:ci,
+      paths:[
+        '2880-1800-crop-ferrari-250-gt-coupe-pininfarina-ii-c804316082018194649_1.jpg',
+        '001Sintesi.jpg',
+        '8e23fea18e5f18fec80320f805088260.jpg',
+        '12096.jpg',
+        '394096_70b74c24a99a_hd.jpg',
+        '105742745-15502471379612018_11_r1t_1.jpg',
+        '160111123507-lamborghini-miura-02.jpg',
+        'bianca-2.jpg',
+        'Lori-2015-Morgan-Three-Wheeler-05.jpg',
+        'Mclaren-720s.jpg',
+        'plymouth-cuda-hero-image.jpg',
+        'Porsche+356+1600+Super-2.jpg'
+      ],
     };
   }
 
@@ -63,44 +77,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.<br />
         </p>
-        <div class="container">
-          <div class="row">
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Pininfarina Ferrari" onClick={() => this.ChangeBGStyle("images/full/2880-1800-crop-ferrari-250-gt-coupe-pininfarina-ii-c804316082018194649_1.jpg")}>
-              <img src="images/thumbs/2880-1800-crop-ferrari-250-gt-coupe-pininfarina-ii-c804316082018194649_1.jpg" width="100%"alt="" />
+        <div className="container">
+          <div className="row">
+          {this.state.paths.map((item, i) => (
+            <div key={i} className="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Pininfarina Ferrari" onClick={() => this.ChangeBGStyle(`images/full/${item}`)}>
+              <img src={`images/thumbs/${item}`} width="100%"alt="" />
             </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Pininfarina Sintesi" onClick={() => this.ChangeBGStyle("images/full/001Sintesi.jpg")}>
-              <img src="images/thumbs/001Sintesi.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Gunther Werks Porsche 400R" onClick={() => this.ChangeBGStyle("images/full/8e23fea18e5f18fec80320f805088260.jpg")}>
-              <img src="images/thumbs/8e23fea18e5f18fec80320f805088260.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Mission E next to '48 356 Roadster" onClick={() => this.ChangeBGStyle("images/full/12096.jpg")}>
-              <img src="images/thumbs/12096.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="GTO Judge" onClick={() => this.ChangeBGStyle("images/full/394096_70b74c24a99a_hd.jpg")}>
-              <img src="images/thumbs/394096_70b74c24a99a_hd.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Rivian Electric Truck" onClick={() => this.ChangeBGStyle("images/full/105742745-15502471379612018_11_r1t_1.jpg")}>
-              <img src="images/thumbs/105742745-15502471379612018_11_r1t_1.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Lamborghini Miura" onClick={() => this.ChangeBGStyle("images/full/160111123507-lamborghini-miura-02.jpg")}>
-              <img src="images/thumbs/160111123507-lamborghini-miura-02.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Pininfarina Battista" onClick={() => this.ChangeBGStyle("images/full/bianca-2.jpg")}>
-              <img src="images/thumbs/bianca-2.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Morgan 3 Wheelers" onClick={() => this.ChangeBGStyle("images/full/Lori-2015-Morgan-Three-Wheeler-05.jpg")}>
-              <img src="images/thumbs/Lori-2015-Morgan-Three-Wheeler-05.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="McLarens" onClick={() => this.ChangeBGStyle("images/full/Mclaren-720s.jpg")}>
-              <img src="images/thumbs/Mclaren-720s.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="1971 Hemi Cuda" onClick={() => this.ChangeBGStyle("images/full/plymouth-cuda-hero-image.jpg")}>
-              <img src="images/thumbs/plymouth-cuda-hero-image.jpg" width="100%"alt="" />
-            </div>
-            <div class="col-3 mb-5 carimg" data-toggle="tooltip" data-placement="top" title="Porsche 356 Roadster Convertible" onClick={() => this.ChangeBGStyle("images/full/Porsche+356+1600+Super-2.jpg")}>
-              <img src="images/thumbs/Porsche+356+1600+Super-2.jpg" width="100%"alt="" />
-            </div>
+          ))}
           </div>
         </div>
       </div>
